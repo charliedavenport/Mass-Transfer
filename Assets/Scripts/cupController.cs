581cfe986fc7065fdf97f25669603644d5513d34
Melanie <melonbar94@gmail.com>
2018-05-01 23:10:45 -0400

    Cup stuff
    some of the tea stuff - need to add in more things.

---
+++ Assets/Scripts/cupController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupController : MonoBehaviour {
+
+    GameObject tea;
+    GameObject tempButton;
+    Vector3 teaPos;
+    float teaPosY;
+
+    private void Awake()
+    {
+        tea = GameObject.Find("tea");
+        tempButton = GameObject.Find("tempButton");
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        teaPos = tea.transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(tea.transform.position.y);
+    }
+
+    void onCollsionEnter(Collision col)
+    {
+        if (col.gameObject.name == "tempButton")
+        {
+            teaPosY = tea.transform.position.y - 1; ;
+        }
+    }
+}
